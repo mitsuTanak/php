4805413e9eaac03ecd73b67545dcc1ffb66d192a
mitsuTanak <130664643+mitsuTanak@users.noreply.github.com>
2023-08-10 13:25:57 -0300

    uyjthgr

---
+++ Aula_01/sintaxe4.php
@@ -0,0 +1,15 @@
+<body>
+    <!-- Texto em HTML -->
+    <h1>HTML e PHP mesclado</h1>
+    <p>Paragrafo HTML</p>
+
+    <!-- Código com PHP e HTML -->
+
+    <?php
+
+        $linguagem = "Linguagem PHP"
+    ?>
+
+    <p>Parágrafo mesclando HTML com <?=$linguagem?></p>
+
+</body>
