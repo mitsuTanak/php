9aa90f04e913082ae74b8fee02fb15e0878b0700
mitsuTanak <130664643+mitsuTanak@users.noreply.github.com>
2023-08-10 13:36:24 -0300

    gf

---
+++ Lista/Exercicio2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Pedro M. T. Cota -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Titulo -->
+    <title>Sintaxe Geral</title>
+
+    <!-- CSS Interno -->
+    <style>
+        .destaque{color: green;}
+        .html{color: orange;}
+        .php{color: blue}
+    </style>
+</head>
+<body>
+    
+    <!-- Texto em HTML -->
+    <h1 class="destaque">Trabalhando com PHP</h1>
+
+    <p>Programador: Pedro Mitsuaki Tanaka Costa</p>
+
+    <!-- Codigo do PHP -->
+    <?php
+
+        // Geração de Texto (String)    
+        echo "Chama Sesi-Senai";
+
+        /* Geração de Texto Estruturado (Com Tegs, Atributos)*/
+        echo "<h2> Gerando <span class='html'>HTML</span> atravez do PHP.</h2>";
+        echo "<h2> Gerando <span class= \"html\">HTML</span> atravez do PHP.</h2>"
+
+    ?>
+
+    <!-- Texto em HTML -->
+    <h1>HTML e PHP mesclado</h1>
+    <p>Paragrafo HTML</p>
+
+    <!-- Código com PHP e HTML -->
+
+    <?php
+
+        $linguagem = "Linguagem <span class='php'>PHP</span>"
+    ?>
+
+    <p>Parágrafo mesclando<span class="html"> HTML</span> com <?=$linguagem?></p>
+
+</body>
+</html>
